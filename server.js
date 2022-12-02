5c8eb671bdeb15d1e9b4226e5a22bc6c2179e55f
sunu park <sunup4rk@gmail.com>
2022-12-02 13:56:10 +0900

    Setting: 초기 세팅입니다.

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/webtoon', function(req, res) {
-    res.send('웹툰을 서비스 해주는 페이지 입니다.');
-})
-
-app.get('/game', function(req, res) {
-    res.send('게임을 서비스 해주는 페이지 입니다.');
+app.get('/', function(req, res) {
+    res.send('메인페이지 입니다.');
 })
 
 app.listen(8080, function() {
