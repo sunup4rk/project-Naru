c752f663293844681d604d80736d53affe6d0607
hyejin <eunyu0604@gmail.com>
2022-12-15 16:41:43 +0900

    #72 [Feature] 로그인 기능

--- frontend-naru/src/pages/signIn/SignIn.js
+++ frontend-naru/src/pages/signIn/SignIn.js
@@ -1,19 +1,42 @@
+import { useNavigate, Link } from 'react-router-dom';
+import { useForm } from "react-hook-form";
 import { ReactComponent as Logo } from "../../assets/images/logo01.svg";
+import { Modal } from './../../components/common/modal/modal';
 import Button01 from "../../components/common/button/Button01";
 import Input01 from "../../components/common/input/Input01";
 import './SignIn.scss';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const SignIn = () => {
-    
+    const navigate = useNavigate();
+    const { register, handleSubmit} = useForm ();
+    const { Warning, Failure } = Modal();
+
+    const onClickSignIn = (data) =>{
+            axios.post("http://localhost:8080/signin", {
+                email: data.email,
+                password: data.password
+            })
+            .then((response) => {
+                if(response.data.message === "로그인 성공") {
+                    navigate('/')
+                }
+                else {
+                    Warning("로그인 실패", response.data.message);
+                }
+            }).catch((error) => {
+                Failure("로그인 실패", "로그인에 실패했습니다.")
+            })
+    }
+
     return (
         <div className="signIn">
-            <div className="signIn-wrapper --top">
+            <form className="signIn-wrapper --top" onSubmit={handleSubmit(onClickSignIn)}>
                 <Logo height="55" role="img"/>
-                <Input01 type={"text"} placeholder={"이메일"} size={"m"}/>
-                <Input01 type={"password"} placeholder={"비밀번호"} size={"m"}/>
+                <Input01 type={"text"} placeholder={"이메일"} size={"m"} register={register("email")} />
+                <Input01 type={"password"} placeholder={"비밀번호"} size={"m"} register={register("password")}/>
                 <Button01 text={"로그인"} size={"m"}/>
-            </div>
+            </form>
 
             <div className="signIn-wrapper --bottom">
                 <span>회원이 아니신가요?</span>
