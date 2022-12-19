b73210b01d4d0e62416d75a565e10d0ac52cec46
YuuJin72 <kkjo9573@gmail.com>
2022-12-19 15:45:22 +0900

    #116 [Feature] 게시글 검색 기능 추가

--- frontend-naru/src/components/layout/ExploreLayout.js
+++ frontend-naru/src/components/layout/ExploreLayout.js
@@ -1,7 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import  styled  from 'styled-components';
 import { ExploreBanner } from './banner/Banner03';
-import Aside01 from './aside/Aside01';
+import Aside01 from './Aside/Aside01';
+
 
 const Body = styled.div`
 display: flex;
