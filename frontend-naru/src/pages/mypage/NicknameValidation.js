d45fed8e91a0bd771502f29a935c574a36a58068
hhjk00 <eunyu0604@gmail.com>
2022-12-26 21:59:47 +0900

    #213 [Feature] 게시글 작성 취소 기능

--- frontend-naru/src/pages/mypage/NicknameValidation.js
+++ frontend-naru/src/pages/mypage/NicknameValidation.js
@@ -3,5 +3,5 @@ import * as yup from "yup";
 const nicknamePattern = "^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,8}$";
 
 export const schema = yup.object().shape({
-    nickname: yup.string().matches(nicknamePattern, "닉네임은 한글,영문,숫자 포함 2~8자여야 합니다.").required("닉네임을 입력하세요."),
+    nickname: yup.string().required("변경할 닉네임을 입력하세요.").matches(nicknamePattern, "닉네임은 한글,영문,숫자 포함 2~8자여야 합니다."),
   });
